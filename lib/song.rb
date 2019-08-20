075304b24f699a3837536ba13f02086432046040
Zach  Smith <zdsmith001@gmail.com>
2019-08-20 21:34:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,10 +34,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if find_by_name(name)
-      name
-    else
-      create_by_name(name)
+    self.find_by_name(name) or self.create_by_name(name)
     end
   end
 
